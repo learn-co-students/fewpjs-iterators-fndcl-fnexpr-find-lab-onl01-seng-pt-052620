4ee1c0eba2afbd93d6edbb011678323627a76dc7
lsuman83 <root@DESKTOP-3EM91F2.localdomain>
2020-12-10 23:41:23 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+
+function superbowlWin(record){
+  const chiefsWin = record.find(({result}) => result === 'W' )
+  
+  if (!!chiefsWin)
+    return chiefsWin.year
+  else
+    return chiefsWin
+  
+}

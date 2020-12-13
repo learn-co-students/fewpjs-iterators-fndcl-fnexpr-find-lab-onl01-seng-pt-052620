63e61835ede92256b61ec699c477a4d1495cd825
8bitbran <brandonmar97@gmail.com>
2020-12-13 13:32:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function superbowlWin(records) {
   let win = records.find(o => o.result === "W");
+
+  // Alternate method
+  // let win = records.find(function(o) {
+  //   return o.result === "W"
+  // });
   return win ? win.year : undefined;
 }

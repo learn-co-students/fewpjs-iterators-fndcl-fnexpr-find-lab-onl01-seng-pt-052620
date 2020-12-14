8cb69077245c66e7d4748d158e4a967935059c4f
wynterwolf <shinyguns@gmail.com>
2020-12-14 15:26:50 -0600

    initial commit

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+// function superbowlWin = record.find( r=> r.result == "W")
+
+function superbowlWin(record) {
+  let winner = record.find(r => r.result === "W")
+  return winner
+
+}
+
+superbowlWin()

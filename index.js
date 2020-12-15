337946807703932df5541d9bee89a9ea77ab23fc
wynterwolf <shinyguns@gmail.com>
2020-12-14 18:59:06 -0600

    Done.

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function testFunc() {
 // function superbowlWin = record.find( r=> r.result == "W")
 function superbowlWin(record) {
   const winner = record.find(({ result }) => result === "W");
-  return (winner ? winner.year : undefined)
-
+  // return (winner ? winner.year : undefined)<-----works too!
+  return winner && winner.year
 }

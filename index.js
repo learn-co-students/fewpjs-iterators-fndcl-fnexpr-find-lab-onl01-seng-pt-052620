54271a35553cccebacf4579f716f645a955822b5
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2020-12-19 01:11:24 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,24 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+// [1, 3, 5, 6, 8].find(function(e){
+//   return e % 2 === 0
+// })
+
+
+// let roommates = ["jess", "winston", "winston", "nick"]; 
+
+// roommates.find(function (s){
+//   return s === "winston"
+// })
+
+// roommates.filter(function(s){
+//   return s === "winston"
+// })
+
+
+function superbowlWin(record){
+ let result = record.find(record => record.result === "W")
+  return !!result ? result.year : undefined 
+}
